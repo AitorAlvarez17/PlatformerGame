41ed856cb35d13bd46bf0c376acf30ddc7d18426
Telmiyo <telmoberoiz@hotmail.com>
2020-10-31 22:00:45 +0100

    Module Fade to Black
    No esta incluido en el App.cpp & .h

---
+++ Game/Source/ModuleFadeToBlack.h
@@ -0,0 +1,63 @@
+#ifndef __MODULEFADETOBLACK_H__
+#define __MODULEFADETOBLACK_H__
+
+#include "Module.h"
+#include "SDL\include\SDL_rect.h"
+
+
+class ModuleFadeToBlack : public Module
+{
+public:
+	//Constructor
+	ModuleFadeToBlack(bool startEnabled, pugi::xml_node& config);
+
+	//Destructor
+	~ModuleFadeToBlack();
+
+	// Called when the module is activated
+	// Enables the blending mode for transparency
+	bool Start() override;
+
+	// Called at the middle of the application loop
+	// Updates the fade logic
+	bool Update();
+
+	// Called at the end of the application loop
+	// Performs the render call of a black rectangle with transparency
+	bool PostUpdate();
+
+	// Called from another module
+	// Starts the fade process which has two steps, fade_out and fade_in
+	// After the first step, the modules should be switched
+	bool FadeToBlack(Module* toDisable, Module* toEnable, float frames = 60);
+
+private:
+
+	enum Fade_Step
+	{
+		NONE,
+		TO_BLACK,
+		FROM_BLACK
+	} 
+	
+	currentStep = Fade_Step::NONE;
+
+	int fadePause = 0;
+
+	// A frame count system to handle the fade time and ratio
+	Uint32 frameCount = 0;
+	Uint32 maxFadeFrames = 0;
+	int width;
+	int height;
+	int scale;
+
+	// The rectangle of the screen, used to render the black rectangle
+	SDL_Rect screenRect;
+	SDL_Rect firstScreenRect;
+
+	// The modules that should be switched after the first step
+	Module* moduleToEnable = nullptr;
+	Module* moduleToDisable = nullptr;
+};
+
+#endif //__MODULEFADETOBLACK_H__
