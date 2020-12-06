04e72b7012dc53d19914a3ffc990033b3d78a7a1
Telmiyo <iamtelmo@proton.me>
2020-12-06 23:46:19 +0100

    enemy deleted

--- Game/Source/Scene.cpp
+++ Game/Source/Scene.cpp
@@ -51,7 +51,7 @@ bool Scene::Start()
 	app->oManager->AddObject(ObjType::COIN, 3014, 2590, 0);
 	app->oManager->AddObject(ObjType::COIN, 1667, 1850, 0);
 	app->oManager->AddObject(ObjType::COIN, 1537, 1854, 0);
-	app->oManager->AddEnemy(600, 2200);
+	//app->oManager->AddEnemy(600, 2200);
 
 
 	
