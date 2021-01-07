0e9bccc7bf2fd1df7b5debaf17428b07c99c0674
Telmiyo <telmoberoiz@hotmail.com>
2021-01-07 17:54:51 +0100

    Pathfinding Update

--- Game/Source/Pathfinding.cpp
+++ Game/Source/Pathfinding.cpp
@@ -174,12 +174,12 @@ void PathNode::Description(PathNode& node)
 	LOG("f score: %d g: %d h: %d", node.Score(), node.g, node.h);
 }
 
-void PathFinding::DrawPath(Render* render)
+void PathFinding::DrawPath(Render* render, DynArray<iPoint> path)
 {
-	int c = lastPath.Count() ;
+	int c = path.Count() ;
 	for (int i = 0; i < c; i++)
 	{
-		SDL_Rect tmp = { lastPath[i].x * 16,lastPath[i].y * 16 , 16, 16 };
+		SDL_Rect tmp = { path[i].x * 16,path[i].y * 16 , 16, 16 };
 		render->DrawRectangle(tmp, { 255, 0, 0, 255 });
 	}
 
